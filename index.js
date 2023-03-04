a48d3a6eae8a4ccebf074318a8a30400191a45e1
Geepy <geepytechnologies@gmail.com>
2023-03-04 19:28:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("./frontend/dist"));
 //   app.use(express.static("frontend/dist"));
 // }
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/frontend/dist", "index.html"));
 });
 // const filepath = path.resolve(__dirname + "/frontend/dist", "index.html");
