9b320384ced92be2280c585e45be89b999ceede2
Geepy <geepytechnologies@gmail.com>
2023-03-04 19:27:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("frontend/dist"));
+app.use(express.static("./frontend/dist"));
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("frontend/dist"));
